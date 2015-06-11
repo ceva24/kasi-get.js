e527a6484e2e7970ae50c4e79a1bf7f6e2a63cb4
Ceva24 <ceva24@outlook.com>
2015-06-12 00:24:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# kasi-get.js
+A Google Chrome bookmarklet that extracts lyrics from Japanese site [UtaMap](http://www.utamap.com).
+
+## Instructions
+
+1. Bookmark this link: [kasi-get](javascript:(function(\){var%20kasiScript=document.createElement('script'\);kasiScript.src='https://raw.githubusercontent.com/Ceva24/kasi-get/master/kasi-get.js';document.body.appendChild(kasiScript\);}\)(\);).
+2. Go to a page with lyrics.
+3. Click the bookmarklet.
+4. The lyrics will appear in a new tab. **Note**: pop-ups must be enabled for the site for this to work.
---
+++ kasi-get.js
@@ -0,0 +1,36 @@
+javascript:(function(){
+
+    var filepath = "http://www.utamap.com/" + $($("script[language=\"javascript\"")[0]).attr("src");
+
+    $.get(filepath, extractAndDisplay, 'text');
+})();
+
+function extractAndDisplay(data) {
+
+    var lines = extractTextFromPage(data);
+
+    displayTextInNewWindow(lines);
+}
+
+function extractTextFromPage(data) {
+
+    var regex = /context2.fillText\(\'([^)]+)\'/g;
+    var lines = []
+
+    while (line = regex.exec(data))
+        lines.push(line[1]);
+
+    return lines;
+}
+
+function displayTextInNewWindow(lines) {
+
+    var w = window.open();
+
+    $(w.document.body).append("<p>");
+
+    for (var i = 0; i < lines.length; i++)
+        $(w.document.body).append(lines[i] + "<br>");
+
+    $(w.document.body).append("</p>");
+}
