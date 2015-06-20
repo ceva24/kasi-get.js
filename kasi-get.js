0b813b9e572ec3db274240dae1b7fa8894bdffb8
Ceva24 <ceva24@outlook.com>
2015-06-20 23:49:36 +0100

    fixed syntax and added comment

--- kasi-get.js
+++ kasi-get.js
@@ -1,5 +1,6 @@
 javascript:(function(){
 
+	// lyrics are embedded into the page via a javascript file - get it's location
     var filepath = "http://www.utamap.com/" + $($("script[language=\"javascript\"")[0]).attr("src");
 
     $.get(filepath, extractAndDisplay, "text");
@@ -16,7 +17,7 @@ function extractTextFromPage(data) {
 
     var regex = /context2.fillText\(\'([^)]+)\'/g;
 
-    var lines = []
+    var lines = [];
     while (line = regex.exec(data))
         lines.push(line[1]);
 
