87a31023b8c3cafbb8394fa05c91b22ce30fb0ee
Ceva24 <ceva24@outlook.com>
2015-08-27 22:52:27 +0100

    fixed regex to cope with brackets and apostrophes

--- kasi-get.js
+++ kasi-get.js
@@ -15,11 +15,14 @@ function showLyrics(data) {
 
 function extractLyricsFromScript(data) {
 
-    var regex = /context2.fillText\(\'([^)]+)\'/g;
-
     var lines = [];
+
+    // Extract all instances of everything after the first apostrophe in the fillText method parameters until the closing bracket and semi-colon
+    var regex = /context2.fillText\(\'(.+?(?=\);))/g;
+
+    var line;
     while (line = regex.exec(data))
-        lines.push(line[1]);
+        lines.push(line[1].substring(0, line[1].lastIndexOf("'"))); // Text may contain escaped apostrophes, so get all text up to the last apostrophe
 
     return lines;
 }
@@ -29,5 +32,5 @@ function showLyricsInNewWindow(lines) {
     var w = window.open();
 
     for (var i = 0; i < lines.length; i++)
-        $(w.document.body).append(lines[i] + "<br>");
+        $(w.document.body).append(lines[i].replace(/\\'/, "'") + "<br>"); // Data may have escaped apostrophes; remove all instances of the '\' escape character
 }
