69194e3b0f37842572ff1b754e9c56b57e465ca5
Ceva24 <ceva24@outlook.com>
2015-08-27 22:57:53 +0100

    updated comments

--- kasi-get.js
+++ kasi-get.js
@@ -17,12 +17,13 @@ function extractLyricsFromScript(data) {
 
     var lines = [];
 
-    // Extract all instances of everything after the first apostrophe in the fillText method parameters until the closing bracket and semi-colon
+    // Extract all instances of: everything after the first apostrophe in the fillText method arguments until the closing bracket and semi-colon
     var regex = /context2.fillText\(\'(.+?(?=\);))/g;
 
     var line;
     while (line = regex.exec(data))
-        lines.push(line[1].substring(0, line[1].lastIndexOf("'"))); // Text may contain escaped apostrophes, so get all text up to the last apostrophe
+        // Text may contain escaped apostrophes, so get all text up to the last apostrophe to be sure that all of the text is captured
+        lines.push(line[1].substring(0, line[1].lastIndexOf("'")));
 
     return lines;
 }
@@ -32,5 +33,6 @@ function showLyricsInNewWindow(lines) {
     var w = window.open();
 
     for (var i = 0; i < lines.length; i++)
-        $(w.document.body).append(lines[i].replace(/\\'/, "'") + "<br>"); // Data may have escaped apostrophes; remove all instances of the '\' escape character
+        // Text may contain escaped apostrophes, so remove all instances of the escape character
+        $(w.document.body).append(lines[i].replace(/\\'/, "'") + "<br>");
 }
