54ffaad30ec7620484ecde5939c2ede9f9f6ec0f
Ceva24 <ceva24@outlook.com>
2015-08-28 01:14:41 +0100

    updated comments

--- kasi-get.js
+++ kasi-get.js
@@ -1,6 +1,6 @@
 javascript:(function(){
 
-    // lyrics are embedded into the page via a javascript file - get it's location
+    // Lyrics are embedded into the page via a javascript file - get it's location
     var filepath = "http://www.utamap.com/" + $($("script[language=\"javascript\"")[0]).attr("src");
 
     $.get(filepath, showLyrics, "text");
@@ -17,12 +17,12 @@ function extractLyricsFromScript(data) {
 
     var lines = [];
 
-    // Extract all instances of: everything after the first apostrophe in the fillText method arguments until the closing bracket and semi-colon
+    // Extract the full arguments list for every fillText function call (including the arguments after the text itself)
     var regex = /context2.fillText\(\'(.+?(?=\);))/g;
 
     var line;
     while (line = regex.exec(data))
-        // Text may contain escaped apostrophes, so get all text up to the last apostrophe to be sure that all of the text is captured
+        // Text may contain escaped apostrophes, so get all text up to the last apostrophe to be sure that it's all captured
         lines.push(line[1].substring(0, line[1].lastIndexOf("'")));
 
     return lines;
@@ -33,6 +33,6 @@ function showLyricsInNewWindow(lines) {
     var w = window.open();
 
     for (var i = 0; i < lines.length; i++)
-        // Text may contain escaped apostrophes, so remove all instances of the escape character
+        // Remove the escape character from escaped apostrophes
         $(w.document.body).append(lines[i].replace(/\\'/, "'") + "<br>");
 }
