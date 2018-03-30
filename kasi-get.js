8d07532e01e9b982df14e3de9135da391978b5c4
ceva24 <ceva24@gmail.com>
2018-03-30 17:12:05 +0100

    Refactor escaping of apostrophes to extract lyric function, add comment

--- kasi-get.js
+++ kasi-get.js
@@ -17,13 +17,20 @@ function extractLyricsFromScript(data) {
 
     var lines = [];
 
-    // Extract the full arguments list for every fillText function call (including the arguments after the text itself)
+    // Extract the full arguments list for every fillText function call (including the arguments after the lyric itself)
     var regex = /context2.fillText\(\'(.+?(?=\);))/g;
 
     var line;
-    while (line = regex.exec(data))
+    while (line = regex.exec(data)) {
+
         // Text may contain escaped apostrophes, so get all text up to the last apostrophe to be sure that it's all captured
-        lines.push(line[1].substring(0, line[1].lastIndexOf("'")));
+        var lyric = line[1].substring(0, line[1].lastIndexOf("'"));
+
+        // Remove the escape character from escaped apostrophes
+        var unescapedLyric = lyric.replace(/\\'/g, "'");
+
+        lines.push(unescapedLyric);
+    }
 
     return lines;
 }
@@ -33,6 +40,5 @@ function showLyricsInNewWindow(lines) {
     var w = window.open();
 
     for (var i = 0; i < lines.length; i++)
-        // Remove the escape character from escaped apostrophes
-        $(w.document.body).append(lines[i].replace(/\\'/g, "'") + "<br>");
+        $(w.document.body).append(lines[i] + "<br>");
 }
